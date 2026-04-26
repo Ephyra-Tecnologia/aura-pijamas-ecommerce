79e87ea7cbceb0bc790cc6ae5fd9fe4ce7a3b9f7
João Conche <joao@ephyra.com.br>
2026-04-26 12:32:37 -0300

    chore: initial project setup

---
+++ prisma.config.ts
@@ -0,0 +1,17 @@
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join("prisma", "schema.prisma"),
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+  migrate: {
+    adapter: async () => {
+      const { PrismaPg } = await import("@prisma/adapter-pg");
+      return new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    },
+  },
+});
