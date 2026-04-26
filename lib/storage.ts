8ffa202d1774af6c6569d856dc8f53be150105b1
heyconche <joaoconche@hotmail.com>
2026-04-26 14:34:20 -0300

    feat: admin CRUD de produtos

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,10 +1,12 @@
 import { Storage } from '@google-cloud/storage'
 
+const privateKey = process.env.GCS_PRIVATE_KEY?.replace(/\\n/g, '\n')
+
 const storage = new Storage({
   projectId: process.env.GCS_PROJECT_ID,
   credentials: {
     client_email: process.env.GCS_CLIENT_EMAIL,
-    private_key: process.env.GCS_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: privateKey,
   },
 })
 
@@ -21,7 +23,10 @@ export async function uploadImage(
   await blob.save(file, {
     contentType: mimetype,
     public: true,
+    gzip: false,
   })
+
+  await blob.makePublic()
 
   return `https://storage.googleapis.com/${process.env.GCS_BUCKET_NAME}/${uniqueName}`
 }
